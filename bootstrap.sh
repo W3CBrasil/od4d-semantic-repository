16a3e30fa1b46505c43c9225d66731c6090bb4aa
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-03 19:14:19 -0300

    Adding ruby as a dependency in bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,14 @@ PROJECT_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt-get remove -y openjdk-6-jre
 sudo apt-get install -y openjdk-7-jre
+
+sudo apt-get install -y build-essential nodejs libsqlite3-dev
+
+sudo apt-get remove -y ruby1.9 ruby1.9.1
+sudo apt-get install -y ruby2.1 ruby2.1-dev
+
+sudo gem install bundle
+
+pushd $PROJECT_FOLDER &> /dev/null
+bundle install
+popd &> /dev/null
