1f6506d4369dab47d3e34cd4fb9f80d9799c8305
Rafael Magrin <rafael.magrin@gmail.com>
2014-07-03 16:51:00 +0000

    Modified deploy.sh to make it easier to deploy localy.

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,45 @@
 #!/usr/bin/env bash
 
-if [ "$OD4D_PROD_SERVER" == "" ]; then
-	echo "Please set the server address using the environment variable OD4D_PROD_SERVER"
-	exit 1
-fi
-
-CONNECTION_STRING=od4d@$OD4D_PROD_SERVER
 PACKAGE_FILE=fuseki-package.tar.gz
+TEMP_FOLDER=/tmp
+
+read -d '' DEPLOY_CMD <<EOF
+	cd $TEMP_FOLDER
+	tar zxvf $PACKAGE_FILE
+	cd $TEMP_FOLDER/fuseki-config
+	./install.sh
+	cd ..
+	rm $PACKAGE_FILE && rm -r fuseki-config
+EOF
+
+function deploy_to_localhost {
+	echo "Deploying semantic repository to localhost"
+	CONNECTION_STRING=127.0.0.1
+
+	cp $PACKAGE_FILE $TEMP_FOLDER
+	sh -c "$DEPLOY_CMD"
+}
+
+function deploy_to_production {
+	if [ "$OD4D_PROD_SERVER" == "" ]; then
+		echo "Please set the server address using the environment variable OD4D_PROD_SERVER"
+		exit 1
+	fi
+
+	echo "Deploying semantic repository to $OD4D_PROD_SERVER"
+	CONNECTION_STRING=od4d@$OD4D_PROD_SERVER
+
+	scp $PACKAGE_FILE $CONNECTION_STRING:$TEMP_FOLDER
+	ssh $CONNECTION_STRING "$DEPLOY_CMD"
+}
 
 if [ ! -f "$PACKAGE_FILE" ]; then
 	echo "File $PACKAGE_FILE not found."
 	echo "Please call this is script from the same directory where the $PACKAGE_FILE is locate."
-fi 
+fi
 
-scp $PACKAGE_FILE $CONNECTION_STRING:~/
-ssh $CONNECTION_STRING 'tar zxvf fuseki-package.tar.gz'
-ssh $CONNECTION_STRING 'cd fuseki-config && ./install.sh'
-ssh $CONNECTION_STRING 'rm fuseki-package.tar.gz && rm -r fuseki-config'
+if [ "$1" == "production" ]; then
+	deploy_to_production
+else
+	deploy_to_localhost
+fi
