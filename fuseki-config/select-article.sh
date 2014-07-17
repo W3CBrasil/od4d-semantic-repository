d79a5800fd8e58ff4f0cc548d972a88dca547677
Marcelo Vargas <marcelo.vargas90@gmail.com>
2014-07-17 18:27:54 +0000

    Added fuseki scripts to select articles and partners.

---
+++ fuseki-config/select-article.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+pushd "/opt/od4d/fuseki"
+
+URL=http://localhost:3030/articles
+
+./s-query --service $URL/query '
+PREFIX schema: <http://schema.org/>
+SELECT  ?s ?p ?o
+WHERE   { ?s a schema:Article .
+          ?s ?p ?o
+        }
+'
+
+popd
