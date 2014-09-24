6720b811353b7a2e45765c1ed8281581f666cdad
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-09-24 16:57:18 -0300

    W3CBrasil/OD4D#46

---
+++ fuseki-config/filter-article.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+pushd "/opt/od4d/fuseki"
+
+URL=http://localhost:3030/articles
+
+./s-query --service $URL/query '
+PREFIX schema: <http://schema.org/>
+SELECT DISTINCT ?article ?url ?author ?headline ?summary ?description ?articleBody ?articleSection ?datePublished ?publisher
+WHERE   { ?article a schema:Article .
+          OPTIONAL { ?article schema:url    ?url } .
+          OPTIONAL { ?article schema:author ?author } .
+          OPTIONAL { ?article schema:headline ?headline } .
+          OPTIONAL { ?article schema:summary  ?summary } .
+          OPTIONAL { ?article schema:description ?description } .
+          OPTIONAL { ?article schema:articleBody ?articleBody } .
+          OPTIONAL { ?article schema:articleSection ?articleSection } .
+          OPTIONAL { ?article schema:datePublished ?datePublished } .
+          OPTIONAL { ?article schema:publisher ?publisher } .
+          OPTIONAL { ?article schema:about ?about } .
+          FILTER (?'$1' = "'$2'") .
+        }'
+
+popd
+
+
