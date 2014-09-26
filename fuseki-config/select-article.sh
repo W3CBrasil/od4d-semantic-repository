e6b414324b51112f30ecbeb98f8004d750b5134c
Alvaro Cavalcanti <alvarovictor@gmail.com>
2014-09-26 18:24:50 -0300

    Adding od4d proper URL

--- fuseki-config/select-article.sh
+++ fuseki-config/select-article.sh
@@ -6,18 +6,20 @@ URL=http://localhost:3030/articles
 
 ./s-query --service $URL/query '
   PREFIX schema: <http://schema.org/>
-  SELECT ?article ?url ?author ?headline ?summary ?description ?articleBody ?articleSection ?datePublished ?publisher
-  WHERE   { ?article a schema:Article .
-            ?article schema:url    ?url .
-            ?article schema:author ?author .
-            OPTIONAL { ?article schema:headline ?headline } .
-            OPTIONAL { ?article schema:summary  ?summary } .
-            OPTIONAL { ?article schema:description ?description } .
-            OPTIONAL { ?article schema:articleBody ?articleBody } .
-            OPTIONAL { ?article schema:articleSection ?articleSection } .
-            ?article schema:datePublished ?datePublished .
-            ?article schema:publisher ?publisher .
-          }
+            SELECT DISTINCT ?article ?url ?author ?headline ?summary ?description ?articleBody ?articleSection ?datePublished ?publisher
+            WHERE   { ?article a schema:Article .
+                      OPTIONAL { ?article schema:url    ?url } .
+                      OPTIONAL { ?article schema:author ?author } .
+                      OPTIONAL { ?article schema:headline ?headline } .
+                      OPTIONAL { ?article schema:summary  ?summary } .
+                      OPTIONAL { ?article schema:description ?description } .
+                      OPTIONAL { ?article schema:articleBody ?articleBody } .
+                      OPTIONAL { ?article schema:articleSection ?articleSection } .
+                      OPTIONAL { ?article schema:datePublished ?datePublished } .
+                      OPTIONAL { ?article schema:publisher ?publisher } .
+                      OPTIONAL { ?article schema:about ?about } .
+                      FILTER (?articleSection = "Projetos") .
+                    }
   ORDER BY DESC(?datePublished)
 '
 
